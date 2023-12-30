25b631609897b3d6e3c21a0a538d3f0b2f89da49
Nancy Gahlot <nancygahlot1435@gmail.com>
2023-12-30 18:47:09 +0530

    "change sign in button to sign out "

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,10 @@
+import { useState } from "react";
 import Header from "./Header";
 const Login =()=>{
+    const[isSignInForm,setisSignInForm]=useState(true);
+    const toggleSignForm=()=>{
+           setisSignInForm(!isSignInForm); 
+    }
     return (
         <div>
             <Header/>
@@ -7,10 +12,11 @@ const Login =()=>{
                 <img src="https://assets.nflxext.com/ffe/siteui/vlv3/c31c3123-3df7-4359-8b8c-475bd2d9925d/15feb590-3d73-45e9-9e4a-2eb334c83921/IN-en-20231225-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="no backgroud img"/>
             </div>
             <form className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
-                <h1 className="font-bold text-3xl py-4">Sign In</h1>
+                <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign in": "Sign out"}</h1>
                 <input type ="text" placeholder="Email or phone number" className="p-2 m-2 w-full bg-gray-800"></input>
                 <input type ="password" placeholder="Password" className="p-2 m-2 w-full bg-gray-800"></input>            
-                <button className="p-2 m-2 bg-red-700 w-full rounded-lg">Sign in</button>
+                <button className="p-2 m-2 bg-red-700 w-full rounded-lg">{isSignInForm ? "Sign in": "Sign out"}</button>
+            <p className="py-4"onClick={toggleSignForm}>New to Netflix? Sign up Now</p>
             </form>
         </div>
     )
