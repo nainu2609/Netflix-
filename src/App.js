542b845537b7e67311c46c692b78e46f65a1a334
Nancy Gahlot <nancygahlot1435@gmail.com>
2024-01-05 00:01:52 +0530

    "redux setup"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Body from "./components/Body";
+import { Provider } from "react-redux";
+import appStore from "./utils/appstore";
 function App()
 {
     return (
     <div>
-        <Body/>
+        <Provider store ={appStore}>
+            <Body/>
+        </Provider>
     </div>
     )
 }
