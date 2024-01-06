e0aa0ee998ad79c7db43e81834ba0746ad941bb5
Nancy Gahlot <nancygahlot1435@gmail.com>
2024-01-06 22:56:43 +0530

    "set up redux "

--- src/components/Login.js
+++ src/components/Login.js
@@ -56,7 +56,7 @@ const Login =()=>
                 <img src="https://assets.nflxext.com/ffe/siteui/vlv3/c31c3123-3df7-4359-8b8c-475bd2d9925d/15feb590-3d73-45e9-9e4a-2eb334c83921/IN-en-20231225-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="no backgroud img"/>
             </div>
             <form onSubmit={(e)=>e.preventDefault()}className="absolute w-3/12 p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
-                <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign in": "Sign out"}</h1>
+                <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign in": "Sign Up"}</h1>
                 {! isSignInForm && (
                     <input type ="text" placeholder="Full Name" className="p-2 m-2 w-full bg-gray-800"></input>
                 )}
@@ -64,7 +64,7 @@ const Login =()=>
 
                 <input ref={password}type ="password" placeholder="Password" className="p-2 m-2 w-full bg-gray-800"></input>            
                 <p className="text-red-500 w-full rounded-lg py-2 m-2">{errorMessage}</p>
-                <button className="p-2 m-2 bg-red-700 w-full rounded-lg" onClick={handleButtomClick}>{isSignInForm ? "Sign in": "Sign out"}</button>
+                <button className="p-2 m-2 bg-red-700 w-full rounded-lg" onClick={handleButtomClick}>{isSignInForm ? "Sign in": "Sign Up"}</button>
             <p className="py-4 cursor-pointer"onClick={toggleSignForm}>{isSignInForm ? "New to Netflix? Sign up Now": "Already registered?Sign in Now"}</p>
             </form>
         </div>
