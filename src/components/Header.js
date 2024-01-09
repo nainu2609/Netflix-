6e3fceeb95d7ab146bc30d882eb2d41fe3aa5ea0
Nancy Gahlot <nancygahlot1435@gmail.com>
2024-01-09 23:50:05 +0530

    "navigation correction "

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
-import { signOut } from "firebase/auth";
+import { signOut ,onAuthStateChanged} from "firebase/auth";
 import {auth } from "../utils/firebase";
 import { useNavigate }from"react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { addUser,removeUser } from "../utils/userSlice";
 const Header =()=>{
-    const navigate=useNavigate(); 
+    const dispatch= useDispatch();
+    const navigate= useNavigate();
     const user =useSelector(store=>store.user);
-    const handleSignOut =()=>{
+    const handleSignOut =()=>
+    {
         signOut(auth).then(() => {
             navigate('/');
           }).catch((error) => {
@@ -13,6 +17,20 @@ const Header =()=>{
           });
           
     }
+    useEffect(()=>
+    {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const {uid,email,displayName}=user;
+                dispatch(addUser({uid:uid,email:email,displayName:displayName}));
+            navigate("/browse");
+            } else {
+              dispatch(removeUser());
+              navigate("/");
+              
+            }
+          });
+    },[]);
     return (
         <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
             <img className="w-44"
