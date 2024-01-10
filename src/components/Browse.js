7cb8e8aa096a2cf04186001fefc50faaa34470f3
Nancy Gahlot <nancygahlot1435@gmail.com>
2024-01-10 23:53:51 +0530

    "tmdb api "

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,20 @@
+import { useEffect } from "react";
 import Header from "./Header";
-
+import {API_options} from "../utils/constants";
 const Browse=()=>{
+
+
+    const getNowPlayingMovies = async()=>{
+        const data = await fetch(
+            "https://api.themoviedb.org/3/movie/now_playing?/page=1",
+            API_options
+          );
+          const json = await data.json();
+          console.log(json);
+    }
+    useEffect(()=>{
+        getNowPlayingMovies();
+    },[]);
     return (
         <div>
             <Header/>
