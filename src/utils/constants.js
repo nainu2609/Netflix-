3c4ff34e2b02f618658fb2728fb49e63991c91ed
Nancy Gahlot <nancygahlot1435@gmail.com>
2024-01-10 23:38:08 +0530

    "add constant file"

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const netflix_logo = "https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png"
+
+export const newtflix_background = "https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/6b2842d1-2339-4f08-84f6-148e9fcbe01b/IN-en-20231218-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+
+export const userIcon = "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png";
+
+export const API_options =  {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYzYyYzA5OGU5Y2EzOTQ3N2E5MWU1ZGE3NDZhNmM5ZSIsInN1YiI6IjY1OGE3ZDFjZTAzOWYxNTY5Y2JkZTRiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rAIaVJ_zhEPLbZb7-e18Byse4IyQehbwsq8E3dGwHYQ'
+    }
+};
+
+export const MoviesPosterPathUrl = "https://image.tmdb.org/t/p/w500"
