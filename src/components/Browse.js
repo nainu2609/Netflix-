62dae5d36b3f7bdb6d49946da53529fdc3e5747e
Nancy Gahlot <nancygahlot1435@gmail.com>
2024-01-11 23:18:03 +0530

    "some code correction"

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,15 +2,13 @@ import { useEffect } from "react";
 import Header from "./Header";
 import {API_options} from "../utils/constants";
 const Browse=()=>{
-
-
     const getNowPlayingMovies = async()=>{
         const data = await fetch(
             "https://api.themoviedb.org/3/movie/now_playing?/page=1",
             API_options
           );
           const json = await data.json();
-          console.log(json);
+          
     }
     useEffect(()=>{
         getNowPlayingMovies();
