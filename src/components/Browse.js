89cdfc752b4edc46959ffc793820552ed0f379a7
Nancy Gahlot <nancygahlot1435@gmail.com>
2024-01-15 21:45:24 +0530

    "cleaning the brower component"

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,17 +2,7 @@ import { useEffect } from "react";
 import Header from "./Header";
 import {API_options} from "../utils/constants";
 const Browse=()=>{
-    const getNowPlayingMovies = async()=>{
-        const data = await fetch(
-            "https://api.themoviedb.org/3/movie/now_playing?/page=1",
-            API_options
-          );
-          const json = await data.json();
-          
-    }
-    useEffect(()=>{
-        getNowPlayingMovies();
-    },[]);
+
     return (
         <div>
             <Header/>
